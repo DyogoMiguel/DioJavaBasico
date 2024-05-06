d71719c60cfd3dc04c33ac503c2ba77828f93bcc
Dyogo <dyogofloriani@gmail.com>
2024-05-06 19:28:08 -0300

    Adiciona informações para TiposVariaveis.java

--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double salarioMinimo = 2500;
+
+        short numerocurto = 3;
+
+        int numero = 5;
+
+        final double VALOR_DE_PI = 3.14;
+
+        String texto = "Alguns números aleatórios: ";
+
+        System.out.println(texto + salarioMinimo + numero + numerocurto + VALOR_DE_PI);
     }
 }
